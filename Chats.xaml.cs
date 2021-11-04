41f4dc0d4bbcdb6a5921d10c035813b0dfc427cf
Amisi Jospin Hassan <40860198+AmisiJospin@users.noreply.github.com>
2021-11-04 12:23:02 +0200

    This was the first version of the application

---
+++ Chats.xaml.cs
@@ -0,0 +1,39 @@
+﻿using ImageCircle.Forms.Plugin.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RocApp
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Chats : ContentPage
+	{
+		public Chats ()
+		{
+			InitializeComponent ();
+            ChatList.ItemSelected += ChatList_ItemSelected;
+            LoadList();
+        }
+
+        private void ChatList_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+           
+        }
+       
+        public void LoadList()
+        {
+          //Loading Offline list That Displays Users Info.
+          //Note: This Will require online Data So I Used A Sample list For trial.
+            List<ChatsProperties> ListOfChats = new List<ChatsProperties>();
+            ListOfChats.Add(new ChatsProperties(ImageSource.FromUri(new Uri("https://weneedfun.com/wp-content/uploads/2016/07/Girls-Black-and-White-Profile-Pictures-1.jpg")), "Jacky", "Hi Jack How Is It?", "12:00pm", "1"));
+            ListOfChats.Add(new ChatsProperties(ImageSource.FromUri(new Uri("https://erc.europa.eu/sites/default/files/images/researchers/Profile%20photo%20-%20JBouyer.jpg")), "Jean David", "When are We Meeting?", "12:00pm", "1"));
+            ListOfChats.Add(new ChatsProperties(ImageSource.FromUri(new Uri("https://weneedfun.com/wp-content/uploads/2016/07/Girls-Black-and-White-Profile-Pictures-8.jpg")), "Aline Scott", "Are We Still Meeting tommorrow", "12:00pm", "1"));
+            ChatList.ItemsSource = ListOfChats;
+        }
+    }
+}
---
+++ HomePage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RocApp
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class HomePage : ContentPage
+	{
+		public HomePage ()
+		{
+			InitializeComponent ();
+            Social_Tap.Tapped += Social_Tap_Tapped;
+            Messages_Tap.Tapped += Messages_Tap_Tapped;
+            //Calling The Chat Page Inside The HomePage as The initial Page
+            var page = new Chats();
+            Holder.Content = page.Content;
+            MessageBox.IsVisible = true;
+            SocialBox.IsVisible = false;
+        }
+
+        private void Messages_Tap_Tapped(object sender, EventArgs e)
+        {
+            //Calling The Chat page When Its Clicked.
+            var page = new Chats();
+            Holder.Content = page.Content;
+            MessageBox.IsVisible = true;
+            SocialBox.IsVisible = false;
+        }
+
+        private void Social_Tap_Tapped(object sender, EventArgs e)
+        {
+            //Calling The Social Page When Its Clicked.
+            var page = new SocialPage();
+            Holder.Content = page.Content;
+            MessageBox.IsVisible = false;
+            SocialBox.IsVisible = true;
+        }
+    }
+}
